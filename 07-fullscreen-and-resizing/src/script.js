57a5050f2fb73231d8f855cac49f8dc48f715312
Nehi <dawid.pospula@gmail.com>
2025-02-01 16:12:03 +0100

    07 refactoring

--- 07-fullscreen-and-resizing/src/script.js
+++ 07-fullscreen-and-resizing/src/script.js
@@ -1,26 +1,29 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-/**
- * Base
- */
+/////////////////////////////////////////////////
+// BASE
+/////////////////////////////////////////////////
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Object
- */
+/////////////////////////////////////////////////
+// OBJECT
+/////////////////////////////////////////////////
+
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-/**
- * Sizes
- */
+/////////////////////////////////////////////////
+// SIZES
+/////////////////////////////////////////////////
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
@@ -45,23 +48,18 @@ window.addEventListener('dblclick', () => {
     const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
 
     if(!fullscreenElement) { // if not null
-        if(canvas.requestFullscreen) {
-            canvas.requestFullscreen()
-        } else if (canvas.webkitRequestFullscreen) {
-            canvas.webkitFullscreenElement()
-        }
+        if(canvas.requestFullscreen) canvas.requestFullscreen()
+        else if(canvas.webkitRequestFullscreen) canvas.webkitFullscreenElement()
     } else {
-        if(document.exitFullscreen) {
-            document.exitFullscreen()
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen()
-        }
+        if(document.exitFullscreen) document.exitFullscreen()
+        else if(document.webkitExitFullscreen) document.webkitExitFullscreen()
     }
 })
 
-/**
- * Camera
- */
+/////////////////////////////////////////////////
+// CAMERA
+/////////////////////////////////////////////////
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
@@ -71,17 +69,19 @@ scene.add(camera)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**
- * Renderer
- */
+/////////////////////////////////////////////////
+// RENDERER
+/////////////////////////////////////////////////
+
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 
-/**
- * Animate
- */
+/////////////////////////////////////////////////
+// ANIMATE
+/////////////////////////////////////////////////
+
 const clock = new THREE.Clock()
 
 const tick = () =>
