e6a04ea0f0f7fda97be82ae6657e41a9cdda0b75
Nehi <dawid.pospula@gmail.com>
2025-02-17 09:40:29 +0100

    12 finished

--- 12-3d-text/src/script.js
+++ 12-3d-text/src/script.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import GUI from 'lil-gui'
+// import typefaceFont from '/examples/fonts/helvetiker_regular.typeface.json'
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
 /**
  * Base
@@ -18,16 +21,76 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png', texture => {
+    console.log(`texture id=${texture.id} loaded`)
+})
+matcapTexture.colorSpace = THREE.SRGBColorSpace
+
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader()
+fontLoader.load('fonts/helvetiker_regular.typeface.json', font => {
+    const textGeometry = new TextGeometry(
+        'sraken',
+        {
+            font: font,
+            size: 0.5,
+            depth: 0.2,
+            curveSegments: 3,
+            bevelEnabled: true,
+            bevelThickness: 0.03,
+            bevelSize: 0.02,
+            bevelOffset: 0,
+            bevelSegments: 3,
+        }
+    )
+
+    textGeometry.computeBoundingBox()
+    
+    const helper = new THREE.Box3Helper(textGeometry.boundingBox, 0xffff00);
+    scene.add(helper);
+    console.log(textGeometry.boundingBox)
+    
+    // textGeometry.translate(
+    //     - (textGeometry.boundingBox.max.x - textGeometry.parameters.options.bevelSize) * 0.5,
+    //     - (textGeometry.boundingBox.max.y - textGeometry.parameters.options.bevelSize) * 0.5,
+    //     - (textGeometry.boundingBox.max.z - textGeometry.parameters.options.bevelThickness) * 0.5
+    // )
+
+    textGeometry.center()
+    
+    const matcapMaterial = new THREE.MeshMatcapMaterial({
+        matcap: matcapTexture,
+    })
+    const text = new THREE.Mesh(textGeometry, matcapMaterial)
+    
+    scene.add(text)
+    
+    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+
+    for (let i = 0; i < 100; i++) {
+        const donut = new THREE.Mesh(donutGeometry, matcapMaterial)
+        scene.add(donut)
+
+        donut.position.x = (Math.random() - 0.5) * 10
+        donut.position.y = (Math.random() - 0.5) * 10
+        donut.position.z = (Math.random() - 0.5) * 10
+
+        donut.rotation.x = Math.random() * Math.PI
+        donut.rotation.y = Math.random() * Math.PI
+
+        const scale = Math.random()
+        donut.scale.set(scale, scale, scale)
+    }
+})
+
+
 
 /**
  * Object
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
 
-scene.add(cube)
 
 /**
  * Sizes
@@ -51,6 +114,12 @@ window.addEventListener('resize', () =>
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
+
+/**
+ * Helper
+ */
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
 
 /**
  * Camera
